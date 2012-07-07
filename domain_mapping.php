7282b5410b0019a222f1028caf30e85f41a7d4b8
jeremyfelt <jeremy.felt@gmail.com>
2012-07-06 19:25:35 -0700

    code cleanup, comments

--- domain_mapping.php
+++ domain_mapping.php
@@ -39,34 +39,42 @@ function dm_add_pages() {
 add_action( 'admin_menu', 'dm_add_pages' );
 
 function dm_network_pages() {
-	add_submenu_page('settings.php', 'Domain Mapping', 'Domain Mapping', 'manage_options', 'dm_admin_page', 'dm_admin_page');
-	add_submenu_page('settings.php', 'Domains', 'Domains', 'manage_options', 'dm_domains_admin', 'dm_domains_admin');
+	add_submenu_page( 'settings.php', 'Domain Mapping', 'Domain Mapping', 'manage_options', 'dm_admin_page', 'dm_admin_page' );
+	add_submenu_page( 'settings.php', 'Domains', 'Domains', 'manage_options', 'dm_domains_admin', 'dm_domains_admin' );
 }
 add_action( 'network_admin_menu', 'dm_network_pages' );
 
-// Default Messages for the users Domain Mapping management page
-// This can now be replaced by using:
-// remove_action('dm_echo_updated_msg','dm_echo_default_updated_msg');
-// add_action('dm_echo_updated_msg','my_custom_updated_msg_function');
+/**
+ * Default messages for the domain mapping management page. Can be replaced
+ * by removing and re-adding the dm_echo_updated_msg with custom messaging.
+ *
+ * @return NULL output is echoed if applicable
+ */
 function dm_echo_default_updated_msg() {
-	switch( $_GET[ 'updated' ] ) {
-		case "add":
+
+	if ( ! isset( $_GET['updated'] ) )
+		return;
+
+	switch( $_GET['updated'] ) {
+		case 'add':
 			$msg = __( 'New domain added.', 'wordpress-mu-domain-mapping' );
 			break;
-		case "exists":
+		case 'exists':
 			$msg = __( 'New domain already exists.', 'wordpress-mu-domain-mapping' );
 			break;
-		case "primary":
+		case 'primary':
 			$msg = __( 'New primary domain.', 'wordpress-mu-domain-mapping' );
 			break;
-		case "del":
+		case 'del':
 			$msg = __( 'Domain deleted.', 'wordpress-mu-domain-mapping' );
 			break;
 	}
 
-	if ( isset( $msg ) ) echo '<div class="updated fade"><p>' . $msg . '</p></div>';
+	if ( isset( $msg ) )
+		echo '<div class="updated fade"><p>' . $msg . '</p></div>';
+
 }
-add_action('dm_echo_updated_msg','dm_echo_default_updated_msg');
+add_action( 'dm_echo_updated_msg', 'dm_echo_default_updated_msg' );
 
 function maybe_create_db() {
 	global $wpdb;
