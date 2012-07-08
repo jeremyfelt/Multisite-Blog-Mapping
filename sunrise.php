73a524ddfd8484e951c363e9e8d7e73dce8bd606
jeremyfelt <jeremy.felt@gmail.com>
2012-07-07 18:39:11 -0700

    modify a few variables to read truer to what's going on

--- sunrise.php
+++ sunrise.php
@@ -6,41 +6,49 @@ if ( ! defined( 'SUNRISE_LOADED' ) )
 if ( defined( 'COOKIE_DOMAIN' ) )
 	die( 'The constant "COOKIE_DOMAIN" is defined (probably in wp-config.php). Please remove or comment out that define() line.' );
 
-// let the site admin page catch the VHOST == 'no'
-
 //set our custom table name using the WP DB prefix
 $wpdb->dmtable = $wpdb->base_prefix . 'domain_mapping';
 
-$current_domain = $wpdb->escape( $_SERVER[ 'HTTP_HOST' ] );
-$alternate_domain = preg_replace( '|^www\.|', '', $dm_domain );
+//capture the current domain request and if it includes www, strip that out for an alternate
+$requested_domain = $_SERVER[ 'HTTP_HOST' ];
+$alternate_domain = preg_replace( '|^www\.|', '', $requested_domain );
 
-if ( $current_domain != $alternate_domain )
-	$where = $wpdb->prepare( 'domain IN ( %s, %s )', $current_domain, $alternate_domain );
+if ( $requested_domain != $alternate_domain )
+	$where = $wpdb->prepare( 'domain IN ( %s, %s )', $requested_domain, $alternate_domain );
 else
-	$where = $wpdb->prepare( 'domain = %s', $current_domain );
+	$where = $wpdb->prepare( 'domain = %s', $requested_domain );
 
 //suppress errors and capture current suppression setting
 $suppression = $wpdb->suppress_errors();
 
-$domain_mapping_id = $wpdb->get_var( $wpdb->prepare( "SELECT blog_id FROM $wpdb->dmtable WHERE {$where} ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1" ) );
+//get the blog_id from our custom SQL tables that matches the domain requested
+$domain_mapping_blog_id = $wpdb->get_var( $wpdb->prepare( "SELECT blog_id FROM $wpdb->dmtable WHERE {$where} ORDER BY CHAR_LENGTH(domain) DESC LIMIT 1" ) );
 
-//reset suppression setting
+//reset error suppression setting
 $wpdb->suppress_errors( $suppression );
 
-if( $domain_mapping_id ) {
+/**
+ * If we found a blog_id to match the domain above, then we turn to WordPress to get the
+ * remaining bits of info from the standard wp_blogs and wp_site tables. Then we squash
+ * it all together in the $current_site and $current_blog globals so that it is available
+ * for the remaining operations on this page request.
+ */
+if( $domain_mapping_blog_id ) {
 
-	$current_blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE blog_id = %d LIMIT 1", $domain_mapping_id ) );
+	$current_blog = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->blogs WHERE blog_id = %d LIMIT 1", $domain_mapping_blog_id ) );
 
-	$current_blog->domain = $_SERVER[ 'HTTP_HOST' ];
+	//modify the WP DB's version of the domain and path to our domain mapped version
+	$current_blog->domain = $requested_domain;
 	$current_blog->path = '/';
 
-	$blog_id = $domain_mapping_id;
+	$blog_id = $domain_mapping_blog_id;
 	$site_id = $current_blog->site_id;
 
-	define( 'COOKIE_DOMAIN', $_SERVER[ 'HTTP_HOST' ] );
+	define( 'COOKIE_DOMAIN', $requested_domain );
 
 	$current_site = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $wpdb->site WHERE id = %d LIMIT 0,1", $current_blog->site_id ) );
 
+	//this query seems weird
 	$current_site->blog_id = $wpdb->get_var( $wpdb->prepare( "SELECT blog_id FROM $wpdb->blogs WHERE domain = %s AND path = %s LIMIT 1", $current_site->domain, $current_site->path ) );
 
 	if( function_exists( 'get_current_site_name' ) )
